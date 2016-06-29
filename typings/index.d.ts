d43ad07d1ed6d720ddae19715d63069286ad9278
Jesper Stærkær <jesper@starkar.no>
2016-06-30 00:32:28 +0200

    Fixed broken typing structure

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,19 @@
-/// <reference path="globals/hap/hap.d.ts" />
+export class EventEmitter{
+	addChild(emitter:EventEmitter);
+	after(eventName:string, fn:Function);
+	before(eventName:string, fn:Function)
+	protected bubble(eventName:String, e:EventFacade);
+	protected capture(eventName:String, e:EventFacade);
+	emit(eventName:string, ...attrs:Object[]):boolean;
+	fire(eventName:string, ...attrs:(Object|EventFacade)[]):Object;
+	on(eventName:string, fn:Function);
+	once(eventName:string, fn:Function);
+	onceAfter(eventName:string, fn:Function);
+	onceBefore(eventName:string, fn:Function);
+	removeListener(eventName:string, fn:Function):EventEmitter;
+	setParent(emitter:EventEmitter);
+}
+export class EventFacade {
+	constructor(params?:Object);
+	val(value?:Object):Object; 
+}
