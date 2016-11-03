32bb0700c7926208034807af009a00e767a6923d
Jesper Stærkær <jesper@starkar.no>
2016-11-03 14:51:00 +0100

    def

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,19 +1,19 @@
-declare class EventEmitter{
-		addChild(emitter:EventEmitter): EventEmitter;
-		after(eventName:string, fn:Function): EventEmitter;
-		before(eventName:string, fn:Function): EventEmitter;
-		protected bubble(eventName:String, e:EventFacade): EventEmitter;
-		protected capture(eventName:String, e:EventFacade): EventEmitter;
-		emit(eventName:string, ...attrs:Object[]): boolean;
-		fire(eventName:string, ...attrs:(Object|EventFacade)[]): Object;
-		on(eventName:string, fn:Function): EventEmitter;
-		once(eventName:string, fn:Function): EventEmitter;
-		onceAfter(eventName:string, fn:Function): EventEmitter;
-		onceBefore(eventName:string, fn:Function): EventEmitter;
-		removeListener(eventName:string, fn:Function): EventEmitter;
-		setParent(emitter:EventEmitter): EventEmitter;
+export declare class EventEmitter{
+	addChild(emitter:EventEmitter): EventEmitter;
+	after(eventName:string, fn:Function): EventEmitter;
+	before(eventName:string, fn:Function): EventEmitter;
+	protected bubble(eventName:String, e:EventFacade): EventEmitter;
+	protected capture(eventName:String, e:EventFacade): EventEmitter;
+	emit(eventName:string, ...attrs:Object[]): boolean;
+	fire(eventName:string, ...attrs:(Object|EventFacade)[]): Object;
+	on(eventName:string, fn:Function): EventEmitter;
+	once(eventName:string, fn:Function): EventEmitter;
+	onceAfter(eventName:string, fn:Function): EventEmitter;
+	onceBefore(eventName:string, fn:Function): EventEmitter;
+	removeListener(eventName:string, fn:Function): EventEmitter;
+	setParent(emitter:EventEmitter): EventEmitter;
 }
-declare class EventFacade {
+export declare class EventFacade {
 	constructor(params?:Object);
 	val(value?:Object): Object;
-} 
+}
